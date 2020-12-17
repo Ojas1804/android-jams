0e77f26bf91625c7ab2e74cab837aa0477d930c1
Ojas1804 <74397676+Ojas1804@users.noreply.github.com>
2020-12-17 12:05:54 +0530

    Create q1.kt

---
+++ q1.kt
@@ -0,0 +1,18 @@
+fun main() 
+{
+  print("Enter number: ")
+  var num = Integer.valueOf(readLine())
+  val sign: String = if (num>0)
+        {
+          "positive"
+        }
+      else if (num<0)
+        {
+          "negative"
+        }
+      else
+        {
+          "0 is neither positive nor negative"
+        }
+  println(sign)
+}
